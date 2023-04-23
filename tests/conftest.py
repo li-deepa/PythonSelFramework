bb83239c687dd6d37c5a35920a33fb9bb6ee9f21
li-deepa <80793490+li-deepa@users.noreply.github.com>
2023-04-23 16:23:54 -0700

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from Tools.scripts.fixdiv import report
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 
-driver=None
+#driver=None
 def pytest_addoption(parser):
     parser.addoption(
         "--browser_name", action="store", default="chrome")
@@ -27,27 +27,27 @@ def setup(request):
     driver.close()
     return driver
 
-    @pytest.mark.hookwrapper
-    def pytest_runtest_makereport(item):
+@pytest.mark.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
         """
-            Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
-            :param item:
-            """
-        pytest_html = item.config.pluginmanager.getplugin('html')
-        outcome = yield
-        report = outcome.get_result()
-        extra = getattr(report,'extra',[])
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report,'extra',[])
 
-        if report.when == 'call' or report.when == "setup":
-            xfail = hasattr(report,'wasxfail')
-            if (report.skipped and xfail) or (report.failed and not xfail):
-                file_name = report.nodeid.replace("::","_") + ".png"
-                _capture_screenshot(file_name)
-                if file_name:
-                    html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
-                           'onclick="window.open(this.src)" align="right"/></div>' % file_name
-                    extra.append(pytest_html.extras.html(html))
-            report.extra = extra
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report,'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::","_") + ".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
 
-    def _capture_screenshot(name):
-        driver.get_screenshot_as_file(name)
+def _capture_screenshot(name):
+    driver.get_screenshot_as_file(name)
