cb3b665018d7d6d2e71bc2d7f2ebf22af6f9f493
li-deepa <80793490+li-deepa@users.noreply.github.com>
2023-04-22 17:41:05 -0700

    Update test_e2e.py

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,7 +1,7 @@
 # to run in terminal use -- py.test --browser_name chrome
 
-from ..utilities.BaseClass import BaseClass
-from ..pageObjects.HomePage import HomePage
+from PythonSelFramework.utilities.BaseClass import BaseClass
+from PythonSelFramework.pageObjects.HomePage import HomePage
 
 
 class TestOne(BaseClass):
